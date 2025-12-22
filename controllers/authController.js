0982a08f4e6d29fe6c211b0a755f7eb4287a2e0d
Tanush Saha <tanush.saha05@gmail.com>
2025-12-22 14:04:55 +0530

    register and login page implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,15 +32,21 @@ exports.login = async (req,res) => {
     const { email,password } = req.body;
     try {
         let user = await User.findOne({email});
+
         if(!user) return res.status(400).json({message:'Invalid Credintials'});
+
         const isMatch = await bcrypt.compare(password,user.password);
+
         if(!isMatch) return res.status(400).json({ message: "Invalid Credintials" });
 
         const payload = {user : {id : user.id}};
+
         jwt.sign(payload,process.env.JWT_SECRET,{expiresIn : '7d'}, (err,token) =>{
             res.status(200).json({token,user : {id : user.id , name : user.name}})
         })
-    } catch (error) {
         
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server Error')
     }
 }
